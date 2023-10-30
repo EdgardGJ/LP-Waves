443f8c5dbb6e1959ad707a702cff209d5b0065d9
EDGARD GARCIA <teosamir2004@gmail.com>
2023-10-30 16:52:10 -0600

    First compnent installed

--- src/componentes/Fractalcanvas.js
+++ src/componentes/Fractalcanvas.js
@@ -8,11 +8,8 @@ const ParticleCanvas = () =>  {
     let particles = [];
     const maxParticles = 500;
     let cursor;
-    let currentColor = 0;
-    let colors = [];
-    let colorIndex = 0;
-    let colorChangeTimer = 0;
-    let colorChangeInterval = 500; // cambiar cada dos segundo
+    let currentColor = p.color(p.random(255), p.random(255), p.random(255), 150);
+    let nextColorChange = 0;
 
     p.setup = () => {
       p.createCanvas(window.innerWidth, window.innerHeight);
@@ -20,61 +17,73 @@ const ParticleCanvas = () =>  {
 
       cursor = p.createVector(p.width / 2, p.height / 2);
 
-      // Definir una paleta de colores suaves
-      colors = [p.color(255, 0, 0, 150), p.color(0, 255, 0, 150), p.color(0, 0, 255, 150)];
-
       for ( let i = 0; i < maxParticles; i++) {
-        particles.push(new Particle());
+        const delay = i * 200; // cambia cada 200 milisegundos
+        particles.push(new Particle(delay));
       }
     };
 
     p.draw = () => {
       p.background(0);
 
+      // Cambia el color con un cierto retraso
+      if (p.millis() > nextColorChange) {
+        currentColor = p.color(p.random(255), p.random(255), p.random(255), 150);
+        nextColorChange = p.millis() + 2000;
+      }
+
       // Actualiza la posicion del cursor al centro de la pantalla
       cursor.x = p.mouseX || p.width / 2;
       cursor.y = p.mouseY || p.height / 2;
 
-      // Cambia el color suavemnte 
-      if (p.millis() - colorChangeTimer > colorChangeInterval) {
-        colorChangeTimer = p.millis();
-        colorIndex = (colorIndex + 1) % colors.length;
-      }
 
       // Dibuja y actualiza las particulas
       particles.forEach((particle) => {
-        particle.update();
-        particle.display(colors[colorIndex]);
+        particle.update(currentColor);
+        particle.display();
       });
     };
 
     class Particle {
-      constructor() {
+      constructor(delay) {
         this.position = p.createVector(p.random(p.width), p.random(p.height));
         this.velocity = p.createVector(0, 0);
         this.size = p.random(5, 15);
-        this.maxSpeed = 5;
+        this.delay = delay;
+        this.color = currentColor; // Mismo color paratodas las particulas
+        this.maxSpeed = 7;
 
       }
 
 
-      update() {
+      update(color) {
+        if (p.millis() > this.delay) {
+          this.color = color; // Aplica el color actual
+        }
+
         // Calcula la atraccion hacia el cursor
         const direction = p5.Vector.sub(cursor, this.position);
         direction.setMag(0.1);
         this.velocity.add(direction);
 
         // Aplica limites a la velocidad
-        
         this.velocity.limit(this.maxSpeed);
 
         // Actualiza la posicion
         this.position.add(this.velocity);
 
+        // Rebota en los border
+        if (this.position.x < 0 || this.position.x > p.width) {
+          this.velocity.y *= -1;
+        }
+        if (this.position.y < 0 || this.position.y > p.height) {
+          this.velocity.y *= -1;
+        }
+
       }
 
-      display(color) {
-        p.fill(color);
+      display() {
+        p.fill(this.color);
         p.ellipse(this.position.x, this.position.y, this.size, this.size);
       }
     }
